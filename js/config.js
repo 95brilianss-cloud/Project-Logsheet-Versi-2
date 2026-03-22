4738b9d3f34109aeb22cabb32970c85288d753bd
95brilianss-cloud <95brilianss@gmail.com>
2026-03-22 18:00:00 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const APP_VERSION = '2.0.0';
 const APP_NAME = 'Turbine Logsheet Pro';
 
 // URL Google Apps Script Backend (Ganti dengan URL Deployment Anda)
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwQi2_FjAH6rLX3oKeZ7JJbftMiU99NYKhEzfPxPQ_68RHJDMKxaPVyqVDbWbbk46T_/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxkii8_DcNdqoiRfRdurkU6qODAQrhSUneLF29YrxYLNa9RC-Cz2ofnPcfywprTvGZH/exec";
 
 // Konfigurasi Autentikasi
 const AUTH_CONFIG = {
@@ -68,12 +68,22 @@ const AREAS = {
         "Lube Oil 30-EH-6102 (On/Off)", 
         "Lube Oil Cartridge FI-6143 (%)", 
         "Lube Oil Cartridge PI-6148 (mmH2O)", 
+        "Lube Oil Cartridge PI-6149 (mmH2O)", 
         "Lube Oil PI-6145 (kg/cm2)", 
+        "Lube Oil E-6104 (A/B)", 
         "Lube Oil TI-6127 (°C)", 
+        "Lube Oil FIL-6101 (A/B)", 
         "Lube Oil PDI-6146 (Kg/cm2)", 
+        "Lube Oil PI-6143 (Kg/cm2)", 
         "Lube Oil TI-6144 (°C)", 
         "Lube Oil TI-6146 (°C)", 
-        "Lube Oil TI-6145 (°C)"
+        "Lube Oil TI-6145 (°C)", 
+        "Lube Oil FG-6144 (%)", 
+        "Lube Oil FG-6146 (%)", 
+        "Lube Oil TI-6121 (°C)", 
+        "Lube Oil TI-6116 (°C)", 
+        "Lube Oil FG-6121 (%)", 
+        "Lube Oil FG-6116 (%)"
     ],
     "Control Oil": [
         "Control Oil 30-TK-6103 LI-6106 (%)", 
@@ -82,36 +92,120 @@ const AREAS = {
         "Control Oil FIL-6103 (A/B)", 
         "Control Oil PI-6152 (Bar)"
     ],
+    "Shaft Line": [
+        "Jacking Oil 30-P-6105 PI-6158 (Bar)", 
+        "Jacking Oil 30-P-6105 PI-6161 (Bar)", 
+        "Electrical Turning Gear U-6103 (Remote/Running/Stop)", 
+        "EH-6101 (ON/OFF)"
+    ],
+    "Condenser 30-E-6102": [
+        "LG-6102 (%)", 
+        "30-P-6101 (A/B)", 
+        "30-P-6101 Suction (kg/cm2)", 
+        "30-P-6101 Discharge (kg/cm2)", 
+        "30-P-6101 Load (Ampere)"
+    ],
+    "Ejector": [
+        "J-6101 PI-6126 A (Kg/cm2)", 
+        "J-6101 PI-6127 B (Kg/cm2)", 
+        "J-6102 PI-6128 A (Kg/cm2)", 
+        "J-6102 PI-6129 B (Kg/cm2)", 
+        "J-6104 PI-6131 (Kg/cm2)", 
+        "J-6104 PI-6138 (Kg/cm2)", 
+        "PI-6172 (kg/cm2)", 
+        "LPS Extrac 30-TP-6101 TI-6155 (°C)", 
+        "from U-6102 TI-6104 (°C)"
+    ],
+    "Generator Cooling Water": [
+        "Air Cooler PI-6124 A (Kg/cm2)", 
+        "Air Cooler PI-6124 B (Kg/cm2)", 
+        "Air Cooler TI-6113 A (°C)", 
+        "Air Cooler TI-6113 B (°C)", 
+        "Air Cooler PI-6125 A (Kg/cm2)", 
+        "Air Cooler PI-6125 B (Kg/cm2)", 
+        "Air Cooler TI-6114 A (°C)", 
+        "Air Cooler TI-6114 B (°C)"
+    ],
+    "Condenser Cooling Water": [
+        "Condenser PI-6135 A (Kg/cm2)", 
+        "Condenser PI-6135 B (Kg/cm2)", 
+        "Condenser TI-6118 A (°C)", 
+        "Condenser TI-6118 B (°C)", 
+        "Condenser PI-6136 A (Kg/cm2)", 
+        "Condenser PI-6136 B (Kg/cm2)", 
+        "Condenser TI-6119 A (°C)", 
+        "Condenser TI-6119 B (°C)"
+    ],
     "BFW System": [
         "Condensate Tank TK-6201 (%)", 
         "Condensate Tank TI-6216 (°C)", 
         "P-6202 (A/B)", 
         "P-6202 Suction (kg/cm2)", 
         "P-6202 Discharge (kg/cm2)", 
+        "P-6202 Load (Ampere)", 
         "Deaerator LI-6202 (%)", 
         "Deaerator TI-6201 (°C)", 
         "30-P-6201 (A/B)", 
-        "Condensate Drum 30-D-6201 LI-6209 (%)"
+        "30-P-6201 Suction (kg/cm2)", 
+        "30-P-6201 Discharge (kg/cm2)", 
+        "30-P-6201 Load (Ampere)", 
+        "30-C-6202 A (ON/OFF)", 
+        "30-C-6202 A (Ampere)", 
+        "30-C-6202 B (ON/OFF)", 
+        "30-C-6202 B (Ampere)", 
+        "30-C-6202 PCV-6216 (%)", 
+        "30-C-6202 PI-6107 (kg/cm2)", 
+        "Condensate Drum 30-D-6201 LI-6209 (%)", 
+        "Condensate Drum 30-D-6201 PI-6218 (kg/cm2)", 
+        "Condensate Drum 30-D-6201 TI-6215 (°C)"
+    ],
+    "Chemical Dosing": [
+        "30-TK-6205 LI-6204 (%)", 
+        "30-TK-6205 30-P-6205 (A/B)", 
+        "30-TK-6205 Disch (kg/cm2)", 
+        "30-TK-6205 Stroke (%)", 
+        "30-TK-6206 LI-6206 (%)", 
+        "30-TK-6206 30-P-6206 (A/B)", 
+        "30-TK-6206 Disch (kg/cm2)", 
+        "30-TK-6206 Stroke (%)", 
+        "30-TK-6207 LI-6208 (%)", 
+        "30-TK-6207 30-P-6207 (A/B)", 
+        "30-TK-6207 Disch (kg/cm2)", 
+        "30-TK-6207 Stroke (%)"
     ]
 };
 
-// Struktur Area Cooling Tower Logsheet
+// Struktur Area CT Logsheet
 const AREAS_CT = {
     "BASIN SA": [
         "D-6511 LEVEL BASIN",
+        "D-6511 BLOWDOWN",
         "D-6511 PH BASIN", 
+        "D-6511 TRASSAR (A/M)", 
+        "TK-6511 LEVEL ACID", 
+        "FIL-6511 (A/B)", 
         "30-P-6511 A PRESS (kg/cm2)", 
         "30-P-6511 B PRESS (kg/cm2)", 
+        "30-P-6511 C PRESS (kg/cm2)", 
         "MT-6511 A STATUS", 
-        "MT-6511 B STATUS"
+        "MT-6511 B STATUS", 
+        "MT-6511 C STATUS", 
+        "MT-6511 D STATUS"
     ], 
     "BASIN SU": [
         "D-6521 LEVEL BASIN",
+        "D-6521 BLOWDOWN",
         "D-6521 PH BASIN", 
+        "D-6521 TRASSAR (A/M)", 
+        "TK-6521 LEVEL ACID", 
+        "FIL-6521 (A/B)", 
         "30-P-6521 A PRESS (kg/cm2)", 
         "30-P-6521 B PRESS (kg/cm2)", 
+        "30-P-6521 C PRESS (kg/cm2)", 
         "MT-6521 A STATUS", 
-        "MT-6521 B STATUS"
+        "MT-6521 B STATUS", 
+        "MT-6521 C STATUS", 
+        "MT-6521 D STATUS"
     ]
 };
 
