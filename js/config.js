0cb2c3646f81997bd85d909d2f8940172b4d711d
95brilianss-cloud <95brilianss@gmail.com>
2026-03-30 22:13:46 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 // ============================================
 // 1. APP CONFIGURATION
 // ============================================
-const APP_VERSION = '2.1.7';
+const APP_VERSION = '2.1.8';
 const APP_NAME = 'Turbine Logsheet Pro';
 
 const AUTH_CONFIG = {
